5fa6d6ae290a391347b4f7d3cbb6bddef67ecb93
MokhtarYan <moalla.m.mokhtar@gmail.com>
2023-06-20 14:30:09 +0200

    Home page added

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="textHome">
+        <h4 className="headerHome">Unlimited Movie, TVs Shows, & More.</h4>
+        <button className="buttHome">Watch Now!</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- client/src/components/headerNav/HeaderNav.js
+++ client/src/components/headerNav/HeaderNav.js
@@ -1,36 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { BsSearch } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import "./HeaderNav.css";
+
 const HeaderNav = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>
+    <div className="headd">
       <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Link to="/">
-            <img
-              src="https://res.cloudinary.com/dqzabjiql/image/upload/v1687132547/310-3108734_roll-set-eps-file-film-rolls-vector-png-removebg-preview_ldkrek.png"
-              style={{ height: "60px", width: "auto" }}
-              alt=""
-            />{" "}
-            CINEVERSE
-          </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link to="/">Home</Nav.Link>
-              <Nav.Link to="/movies">Movies</Nav.Link>
-              <Nav.Link to="/tvShows">TV Shows</Nav.Link>
-              <input type="text" placeholder="Search" />
-              <button>
-                {" "}
-                <BsSearch />
-              </button>
-              <Nav.Link to="signin">Sign In</Nav.Link>
-              <Nav.Link to="/signup">Sign Up</Nav.Link>
+        <Container className="Container">
+          <div className="part1">
+            <Link to="/" className="link1">
+              <img
+                src="https://res.cloudinary.com/dqzabjiql/image/upload/v1687132547/310-3108734_roll-set-eps-file-film-rolls-vector-png-removebg-preview_ldkrek.png"
+                style={{ height: "50px", width: "auto" }}
+                alt=""
+              />{" "}
+              CINEVERSE
+            </Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          </div>
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginLeft: "20px",
+            }}
+          >
+            <Nav
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "700px",
+              }}
+            >
+              <div className="part2">
+                <Link to="/" className="link2">
+                  Home
+                </Link>
+                <Link to="/movies" className="link2">
+                  Movies
+                </Link>
+                <Link to="/tvShows" className="link2">
+                  TV Shows
+                </Link>
+              </div>
+              <div className="part3">
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    width: "fit-content",
+                  }}
+                >
+                  <input type="text" className={show ? "open" : "close"} />
+                  <button
+                    className="searchButton"
+                    onClick={() => setShow(!show)}
+                  >
+                    {" "}
+                    <BsSearch />
+                  </button>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignContent: "center",
+                  }}
+                >
+                  <Link to="signin" className="link3">
+                    Sign In
+                  </Link>
+                  <Link to="/signup" className="link3">
+                    Sign Up
+                  </Link>
+                </div>
+              </div>
               {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
